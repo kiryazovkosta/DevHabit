967c9a7615dc28f54928254fef2d1f167e1174ec
kiryazovkosta <kosta.kiryazov@gmail.com>
2025-04-22 15:19:50 +0300

    Add github actions

--- src/DevHabit.Api/Program.cs
+++ src/DevHabit.Api/Program.cs
@@ -1,8 +1,32 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    .WithTracing(tracerProviderBuilder =>
+        tracerProviderBuilder
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation())
+    .WithMetrics(metricsProviderBuilder =>
+        metricsProviderBuilder
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddRuntimeInstrumentation())
+    .UseOtlpExporter();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeScopes = true;
+    options.IncludeFormattedMessage = true;
+});
 
 WebApplication app = builder.Build();
 
